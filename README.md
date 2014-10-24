9a81a337575b475331abdabc561dbeb6785587a8
wezside <wesley@wezside.co.za>
2014-10-24 22:05:49 +0200

    Scrolling animation added

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ mini-dotmatrix
 ==============
 
 Arduino code for 4x4 LED matrix display. This configuration uses multiplexing with a Shift Register 74hc595.
+
+Animation
+---------
+
+Row to animate: 
+	
+	0110
+
+	0000 0110
+	000 0110
+	00 0110
+	0 0110
+	0110
+   0110 
+  0110 0
+ 0110 00
+0110 000
--- main.cpp
+++ main.cpp
@@ -5,22 +5,31 @@
 
 #include "Arduino.h"
 
-int SER_Pin = 8;   //pin 14 on the 75HC595
-int RCLK_Pin = 9;  //pin 12 on the 75HC595
-int SRCLK_Pin = 10; //pin 11 on the 75HC595
-int counter = 0;
-
-// 0110 1001 1111 1001
-char a[] = {6, 9, 15, 9};
-
 //How many of the shift registers - change this
 #define number_of_74hc595s 1 
 
 //do not touch
 #define numOfRegisterPins number_of_74hc595s * 8
 
+#define SPEED 30
+
+int SER_Pin = 8;   //pin 14 on the 75HC595
+int RCLK_Pin = 9;  //pin 12 on the 75HC595
+int SRCLK_Pin = 10; //pin 11 on the 75HC595
+
+int anime = 0;	
+int counter = 0;
+int shift_right_counter = 4;
+int shift_left_counter = 0;
+
+// 0110 1001 1111 1001
+char a[] = {0, 0, 0, 0};
+char a_copy[] = {6, 9, 15, 9};
+
+// Registers or Shift register pins
 boolean registers[numOfRegisterPins];
 
+// Forward declarations
 void clearRegisters();
 void writeRegisters();
 void row(int i);
@@ -28,18 +37,15 @@ void col(int i);
 
 void setup()
 {
-/*	for (int i = 0; i < 4; ++i)
-	{
-		a[i] = a[i] >> 4;
-	}*/
+	Serial.begin(115200);
+
 	pinMode(SER_Pin, OUTPUT);
 	pinMode(RCLK_Pin, OUTPUT);
 	pinMode(SRCLK_Pin, OUTPUT);
 
-	//reset all register pins
+	// Reset all register pins
 	clearRegisters();
-	writeRegisters();
-	Serial.begin(115200);
+	writeRegisters();	
 }               
 
 //set all register pins to LOW
@@ -72,31 +78,46 @@ void setRegisterPin(int index, int value)
 	registers[index] = value;
 }
 
-int anime = 0;	
 void loop()
 {
 	clearRegisters();
 	writeRegisters();
 
+	// Wrap around for row counter
 	if (counter == 4) counter = 0;
+
+	// Light up the row with index
 	row(counter);
 
-/*	if (anime % 100 == 0)
+	// Animate in (right to left)
+	if (anime % SPEED == 0 && shift_right_counter != -1)
 	{
-		Serial.println("Animation");
+		// For each of the rows
 		for (int i = 0; i < 4; ++i)
 		{
-			a[i] = a[i] << 1;
-		}
-		
+			a[i] = a_copy[i] >> shift_right_counter;
+		}		
+		shift_right_counter--;
 	}
-	anime++;*/
+
+	// Animate out (right to left)
+	if (anime % SPEED == 0 && shift_left_counter <= 4 && shift_right_counter == -1)
+	{
+		// For each of the rows
+		for (int i = 0; i < 4; ++i)
+		{
+			a[i] = a_copy[i] << shift_left_counter;
+		}		
+		shift_left_counter++;
+	}
+
+	// Animation frame counter
+	anime++;
 
 	for (int i = 0; i < 4; ++i)
 	{
 		if (!(a[counter] & 1 << i)) col(i);
 	}
-
 	writeRegisters();
 	delay(5);  
 
